3a7ccf92b84e981e7b99856bb3f078dbcaf2ff7a
Wai <tinaung.w@husky.neu.edu>
2017-04-25 16:40:56 -0700

    new

--- src/writeboard.ext/Controllers/External.cs
+++ src/writeboard.ext/Controllers/External.cs
@@ -9,6 +9,16 @@ namespace writeboard.ext.Controllers
     {
         public IActionResult Index()
         {
+            string appURL = "";
+            if (HttpContext.Current.Request.URL == "http://writeboard.net")
+            {
+                ViewBag.appURL = "http:app//writeboard.net"
+            }
+
+            else
+            {
+                ViewBag.appURL = "writeboard-app-tst.azurewebsites.net"
+            }
             SqlConnection conn = new SqlConnection("Server=writeboard-db.database.windows.net;Database=writeboard;User Id=wbadmin;Password=neuedu#2017");
             conn.Open();
             SqlCommand cmd = new SqlCommand("SELECT COUNT(*)FROM dbo.writeboards", conn);
