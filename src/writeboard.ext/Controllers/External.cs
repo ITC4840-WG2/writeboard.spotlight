27255b431b3ccc740e91dae6a503b35fa9f57737
kelleyj <jkelley3@gmail.com>
2017-04-27 00:02:58 -0400

    Contact us functionality (WRIT-55)

--- src/writeboard.ext/Controllers/External.cs
+++ src/writeboard.ext/Controllers/External.cs
@@ -4,11 +4,16 @@ using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 
+using MimeKit;
+using MailKit.Net.Smtp;
+
 namespace writeboard.ext.Controllers
 {
     public class External : Controller
     {
-        public IActionResult Index()
+        [HttpGet("/{wbAtion?}")]
+        [HttpPost("/{wbAction?}")]
+        public IActionResult Index(string wbAction)
         {
             //get current url, set app url
             string currentURL = getCurrentURL(HttpContext);
@@ -19,6 +24,36 @@ namespace writeboard.ext.Controllers
             else
             {
                 ViewBag.appURL = "http://writeboard-app-tst.azurewebsites.net";
+            }
+
+            //process contact us action
+            if (wbAction == "contact")
+            {
+                //get contact form entries
+                string contactName = HttpContext.Request.Form["wb-name"];
+                string contactEmail = HttpContext.Request.Form["wb-email"];
+                string contactSubject = HttpContext.Request.Form["wb-subject"];
+                string contactMsg = HttpContext.Request.Form["wb-msg"];
+
+                //send successful registration email
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("writeboard-team", "writeboard-team@outlook.com"));
+                message.To.Add(new MailboxAddress("writeboard-team", "writeboard-team@outlook.com"));
+                message.Subject = "WriteBoard - Contact";
+                var bodyBuilder = new BodyBuilder();
+                bodyBuilder.HtmlBody = "<b>" + contactName + " Sent:</b><p><p>" + contactSubject + "<p><p>" + contactMsg + "<p> Respond to: mailto://" + contactEmail;
+                message.Body = bodyBuilder.ToMessageBody();
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.live.com", 587, false);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate("writeboard-team@outlook.com", "neuedu#2017");
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
+
+                Response.Redirect("/");
             }
 
             //get writeboard unique registration count
